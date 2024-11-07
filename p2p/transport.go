14bac614dfb5214f2453dac27c019ddf1647da74
AliSinaDevelo <alisinakarimi.2003@gmail.com>
2024-11-07 15:10:50 +0100

    p2p: define Peer and Transport interfaces

--- p2p/transport.go
+++ p2p/transport.go
@@ -1,10 +1,17 @@
 package p2p
 
-// TODO: represents a remote node
+import "net"
+
+// Peer is a remote endpoint connected over the network.
 type Peer interface {
+	RemoteAddr() net.Addr
+	Close() error
+	IsOutbound() bool
 }
 
-// TODO: handles communications between the nodes
+// Transport listens for inbound peers and can dial outbound connections.
 type Transport interface {
-	listenAndAccept() error
+	ListenAndAccept() error
+	Addr() net.Addr
+	Close() error
 }
